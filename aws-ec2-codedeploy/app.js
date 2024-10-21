69e89ba44b8e85b0aa4f7c0e3337598f550a30b2
jmbustos1 <jmbustos1@uc.cl>
2024-10-20 23:27:49 -0300

    port test 5

--- aws-ec2-codedeploy/app.js
+++ aws-ec2-codedeploy/app.js
@@ -10,6 +10,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+
+
 app.listen(port, () => {
   console.log(`Snake app listening on port ${port}`);
 });
