20cd1b2bf8e08948b73b9a9e3a1b96068a1f240b
jmbustos1 <jmbustos1@uc.cl>
2024-10-26 22:55:50 -0300

    deploy fix 3

--- aws-ec2-codedeploy/app.js
+++ aws-ec2-codedeploy/app.js
@@ -10,8 +10,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
-
 app.listen(port, () => {
   console.log(`Snake app listening on port ${port}`);
 });
